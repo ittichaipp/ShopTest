5855c55612715dd1ccb716b12266a7fec9b1deee
ittichaipp <ittichaip.work@gmail.com>
2022-09-07 21:57:49 +0700

    Add project files.

---
+++ ShopTest1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTest1.Models;
+using System.Diagnostics;
+
+namespace ShopTest1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ ShopTest1/Models/entity/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace ShopTest1.Models.entity
+
+{
+    public class Product
+    {
+        public int ProductID { get; set; } = 0;
+        
+
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+        public string ProductPrice { get; set; } = string.Empty;
+        
+        public string Img1 { get; set; } = string.Empty;
+        public string Img2 { get; set; } = string.Empty;
+        public string Img3 { get; set; } = string.Empty;
+
+    }
+}
